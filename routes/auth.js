ba2f6f732c63b7620ffd745a0c88597ab4155889
belachkar ali <34995109+belachkar@users.noreply.github.com>
2019-07-20 18:31:42 +0100

    Add login functionality

--- routes/auth.js
+++ routes/auth.js
@@ -18,8 +18,8 @@ router.post('/register', async (req, res) => {
 
   try {
 
-    // Checking the existance of the email
-    // TODO: make a uniq index in the database for the email.
+    // Checking if the email exists already
+    // TODO: make a uniq index in the database for the email. this code can then be droped
     const isUserExists = await User.findOne({ email: value.email });
     if (isUserExists) {
       console.log('The email already exists!');
@@ -29,7 +29,7 @@ router.post('/register', async (req, res) => {
     // Hashing the password
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(value.password, salt);
-    
+
     // Saving the new user
     value.password = hashedPass;
     const newUser = new User(value);
@@ -43,12 +43,39 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-  
+
   // Validation of the user input
   const { error, value } = loginValitator(req.body);
   if (error) {
     console.error('FAILED User login:', error.details[0].message);
     return res.status(400).json({ Error: error.name });
+  }
+
+  try {
+
+    // Get the user by email
+    const user = await User.findOne({ email: value.email });
+
+    // Checking the user exists
+    if (!user) {
+      console.log('The email does not exists!');
+      return res.status(400).json({ Error: 'Wrong email or password!' });
+    }
+
+    // Checking the password match
+    const isPassMatch = await bcrypt.compare(value.password, user.password);
+    if (!isPassMatch) {
+      console.log('The password does not match!');
+      return res.status(400).json({ Error: 'Wrong email or password!' });
+    }
+
+    // The user exists and the pass match, must login.
+    console.log('User', user.name, 'Logged in');
+    return res.json({ Message: 'User '+user.name+' Logged in successfuly' });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({ Error: err.message });
   }
 
 
--- validators/auth.js
+++ validators/auth.js
@@ -1,20 +1,18 @@
 const Joi = require('@hapi/joi');
 
+// Fields to validate
+const name = Joi.string().alphanum().min(2).max(255).required();
+const email = Joi.string().email({ minDomainSegments: 2 }).required();
+const password = Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).required();
+
 const registerValitator = (data) => {
-  const schema = Joi.object().keys({
-    name: Joi.string().alphanum().min(2).max(255).required(),
-    password: Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).required(),
-    email: Joi.string().email({ minDomainSegments: 2 }).required()
-  });
+  const schema = Joi.object().keys({ name, email, password });
 
   return Joi.validate(data, schema);
 };
 
 const loginValitator = (data) => {
-  const schema = Joi.object().keys({
-    name: Joi.string().alphanum().min(2).max(255).required(),
-    password: Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).required()
-  });
+  const schema = Joi.object().keys({ email, password });
 
   return Joi.validate(data, schema);
 };
